f51d3a714d3c319585a45e86ab6f58ff99d13791
Nathan Thomas <nwthomas@me.com>
2021-11-07 00:35:29 -0700

    Finalize application for deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,34 @@
 import * as React from "react";
 import twitterLogo from "./assets/twitter-logo.svg";
+import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import { Program, Provider, web3 } from "@project-serum/anchor";
 import "./App.css";
+import idl from "./idl.json";
+import kp from "./keypair.json";
+const { SystemProgram } = web3;
+
+const arr = Object.values(kp._keypair.secretKey);
+const secret = new Uint8Array(arr);
+const baseAccount = web3.Keypair.fromSecretKey(secret);
+
+const programID = new PublicKey(idl.metadata.address);
+
+const network = clusterApiUrl("devnet");
+
+const opts = {
+  preflightCommitment: "processed",
+};
 
 // Constants
 const TWITTER_HANDLE = "nwthomas_";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
-const TEST_GIFS = [
-  "https://64.media.tumblr.com/442513caac35229ccdd5e39fe822d6bf/f5c2981514e757fd-01/s500x750/a644d95d7d0936a19d19c5737e071711edfb489c.gifv",
-  "https://media0.giphy.com/media/3ornjSL2sBcPflIDiU/giphy.gif?cid=790b7611555077c08045202155f9793478aac166ca13dca2&rid=giphy.gif&ct=g",
-  "https://media2.giphy.com/media/3o7abB06u9bNzA8lu8/giphy.gif?cid=790b761117f624da6cf7156e60690e5c6996775a6b388278&rid=giphy.gif&ct=g",
-  "https://64.media.tumblr.com/7845da923c4dc12408bb731c0030a1b6/tumblr_mx5p8wJNty1rk2xpdo1_540.gifv",
-];
+// const TEST_GIFS = [
+//   "https://64.media.tumblr.com/442513caac35229ccdd5e39fe822d6bf/f5c2981514e757fd-01/s500x750/a644d95d7d0936a19d19c5737e071711edfb489c.gifv",
+//   "https://media0.giphy.com/media/3ornjSL2sBcPflIDiU/giphy.gif?cid=790b7611555077c08045202155f9793478aac166ca13dca2&rid=giphy.gif&ct=g",
+//   "https://media2.giphy.com/media/3o7abB06u9bNzA8lu8/giphy.gif?cid=790b761117f624da6cf7156e60690e5c6996775a6b388278&rid=giphy.gif&ct=g",
+//   "https://64.media.tumblr.com/7845da923c4dc12408bb731c0030a1b6/tumblr_mx5p8wJNty1rk2xpdo1_540.gifv",
+// ];
 
 const App = () => {
   const [walletAddress, setWalletAddress] = React.useState(null);
@@ -49,9 +66,25 @@ const App = () => {
     });
   }, []);
 
+  const getGifList = async () => {
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programID, provider);
+      const account = await program.account.baseAccount.fetch(
+        baseAccount.publicKey
+      );
+
+      console.log("Got the account", account);
+      setGifList(account.gifList);
+    } catch (error) {
+      console.log("Error in getGifs: ", error);
+      setGifList(null);
+    }
+  };
+
   React.useEffect(() => {
     if (walletAddress) {
-      setGifList(TEST_GIFS);
+      getGifList();
     }
   }, [walletAddress]);
 
@@ -80,34 +113,98 @@ const App = () => {
     setInputValue(event.target.value);
   };
 
+  const getProvider = () => {
+    const connection = new Connection(network, opts.preflightCommitment);
+    const provider = new Provider(
+      connection,
+      window.solana,
+      opts.preflightCommitment
+    );
+    return provider;
+  };
+
   const sendGif = async () => {
-    if (inputValue.length > 0) {
-      console.log("Gif link:", inputValue);
-    } else {
-      console.log("Empty input. Try again.");
+    if (inputValue.length === 0) {
+      console.log("No gif link given!");
+      return;
+    }
+    console.log("Gif link:", inputValue);
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programID, provider);
+
+      await program.rpc.addGif(inputValue, {
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+        },
+      });
+      console.log("GIF sucesfully sent to program", inputValue);
+
+      await getGifList();
+    } catch (error) {
+      console.log("Error sending GIF:", error);
     }
   };
 
-  const renderConnectedContainer = () => (
-    <div className="connected-container">
-      <input
-        type="text"
-        placeholder="Enter gif link!"
-        value={inputValue}
-        onChange={onInputChange}
-      />
-      <button className="cta-button submit-gif-button" onClick={sendGif}>
-        Submit
-      </button>
-      <div className="gif-grid">
-        {gifList.map((gif) => (
-          <div className="gif-item" key={gif}>
-            <img src={gif} alt={gif} />
-          </div>
-        ))}
+  const createGifAccount = async () => {
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programID, provider);
+      console.log("ping");
+      await program.rpc.startStuffOff({
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+          systemProgram: SystemProgram.programId,
+        },
+        signers: [baseAccount],
+      });
+      console.log(
+        "Created a new BaseAccount w/ address:",
+        baseAccount.publicKey.toString()
+      );
+      await getGifList();
+    } catch (error) {
+      console.log("Error creating BaseAccount account:", error);
+    }
+  };
+
+  const renderConnectedContainer = () => {
+    if (gifList === null) {
+      return (
+        <div className="connected-container">
+          <button
+            className="cta-button submit-gif-button"
+            onClick={createGifAccount}
+          >
+            Do One-Time Initialization For GIF Program Account
+          </button>
+        </div>
+      );
+    }
+
+    return (
+      <div className="connected-container">
+        <input
+          type="text"
+          placeholder="Enter gif link!"
+          value={inputValue}
+          onChange={onInputChange}
+        />
+        <button className="cta-button submit-gif-button" onClick={sendGif}>
+          Submit
+        </button>
+        <div className="gif-grid">
+          {/* We use index as the key instead, also, the src is now item.gifLink */}
+          {gifList.map((item, index) => (
+            <div className="gif-item" key={index}>
+              <img src={item.gifLink} />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  };
 
   return (
     <div className="App">
